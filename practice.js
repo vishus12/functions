df1c1ca5aedd108a339cef9426ca3ddbfa968522
Vishal Trivedi <vishaltrivedi92@gmail.com>
2021-04-14 10:13:24 -0500

    First Half of the walkthrough followed

---
+++ practice.js
@@ -0,0 +1,9 @@
+const calculateAgeInDogYears = (ageOfPerson) => {
+    const ageInDogYears = ageOfPerson / 7
+
+    return ageInDogYears
+}
+
+const dogage = calculateAgeInDogYears(45)
+console.log(dogage)
+
