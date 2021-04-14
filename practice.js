7ef550e14e305a8d2e4b37de72389057cc134bfb
Vishal Trivedi <vishaltrivedi92@gmail.com>
2021-04-14 14:32:54 -0500

    4 parts done but not correct still working out the kinks

--- practice.js
+++ practice.js
@@ -1,9 +1,38 @@
+// Part 1: Functions
 const calculateAgeInDogYears = (ageOfPerson) => {
     const ageInDogYears = ageOfPerson / 7
 
     return ageInDogYears
 }
 
-const dogage = calculateAgeInDogYears(45)
-console.log(dogage)
+const dogAge = calculateAgeInDogYears(45)
+console.log(dogAge)
+
+// Part 2: Best in Show
+const specificDogBreed = (favoriteDogBreed) => {
+    const dogBreed = favoriteDogBreed 
+
+    return dogBreed
+}
+
+const favoriteDog = specificDogBreed()
+console.log(favoriteDog)
+
+// Part 3: Addition
+function add( num1, num2){
+    let res = num1 + num2
+    return num1 + num2
+}
+add(17,4)
+console.log(res)
+
+const add = function(num1, num2) { 
+    return num1 + num2 
+}
+
+// Part 4: Self-Driving Cars
+
+
+
+
 
